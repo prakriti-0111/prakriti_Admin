dd8bf777492700c9896b6f54da6a790afa32b840
Rahul srivastava <rahul24srivastava2001@gmail.com>
2025-11-05 14:37:22 +0530

    Add getStocksList action to fetch stocks data from the server. Update stocks.actions.js with new API endpoint for stock retrieval.

--- src/actions/superadmin/stocks.actions.js
+++ src/actions/superadmin/stocks.actions.js
@@ -91,6 +91,11 @@ export const getCartItemById = async(params) => {
     return await axios.get(`/superadmin/cart/checkdetail${params}`);
 }
 
+export const getStocksList = async(params) => {
+    params = objectToQuery(params, true)
+    return await axios.get(`/superadmin/stocks${params}`);
+}
+
 export const returnStockMoveToStock = async(data) => {
     return await axios.post(`/superadmin/stocks/return-stock/move-to-stock`, data);
 }
